eaf60cb96d90f14b313f27bbe919b5da7583f5b3
Dr Moe <snannann@Moes-MacBook-Air.local>
2017-05-04 20:52:09 -0400

    add objects to js

--- immigrants/main.js
+++ immigrants/main.js
@@ -1,7 +1,53 @@
+var responses = {
+	construction: {
+		libya: "Lorem ipsum",
+		iran: "dolor sit",
+		syria: "amet consectitur",
+		yemen: "Fam quinoa succulents",
+		sudan: "cronut pabst vexillologist butcher",
+		somalia: "keytar lo-fi chillwave tofu synth"
+	},
+
+	manufacturing: {
+		libya: "the song goes",
+		iran: "hey now",
+		syria: "you're an all star",
+		yemen: "get your game on",
+		sudan: "go play",
+		somalia: "by Smash Mouth"
+	}
+};
+
 $(document).ready(function () {
 
-	$("#menu").change(function (e) {
-		$(".country").addClass("active");
+	$("#menu").on("change", function () {
+		var select = $(this);
+		if (select.val() === '0') {
+			$(".country").removeClass("active");
+		} else {
+			$(".country").each(function () {
+				var country = $(this);
+				var name = country.attr("id");
+				var content = country.find(".content");
+				var text = responses[select.val()][name];
+				country.addClass("active");
+				content.html(text);
+			});
+		}
 	});
 
+	$(".country").on("click", function (e) {
+		e.preventDefault();
+
+		var country = $(this);
+
+		if (country.hasClass("open")) {
+			country.find(".content").slideUp();
+			country.removeClass("open");
+		} else {
+			country.find(".content").slideDown();
+			country.addClass('open');
+		}
+
+	});
 });
