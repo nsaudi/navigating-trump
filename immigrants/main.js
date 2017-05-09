eb83820a2bb3968b6d789734f0d1dfef3d12bc34
Prachi Bhardwaj <prachi.bhardwaj@journalism.cuny.edu>
2017-05-09 16:09:01 -0400

    added amounts to countries

--- immigrants/main.js
+++ immigrants/main.js
@@ -1,5 +1,5 @@
 var responses = {
-	construction: {
+	agriculture: {
 		libya: "Lorem ipsum",
 		iran: "dolor sit",
 		syria: "amet consectitur",
@@ -8,14 +8,232 @@ var responses = {
 		somalia: "keytar lo-fi chillwave tofu synth"
 	},
 
-	manufacturing: {
+	construction: {
 		libya: "the song goes",
 		iran: "hey now",
 		syria: "you're an all star",
 		yemen: "get your game on",
 		sudan: "go play",
 		somalia: "by Smash Mouth"
-	}
+	},
+
+	manufacturing: {
+		libya: "Lorem ipsum",
+		iran: "dolor sit",
+		syria: "amet consectitur",
+		yemen: "Fam quinoa succulents",
+		sudan: "cronut pabst vexillologist butcher",
+		somalia: "keytar lo-fi chillwave tofu synth"
+	},
+
+	wholesaletrade: {
+		libya: "the song goes",
+		iran: "hey now",
+		syria: "you're an all star",
+		yemen: "get your game on",
+		sudan: "go play",
+		somalia: "by Smash Mouth"
+	},
+
+	retailtrade: {
+		libya: "the song goes",
+		iran: "hey now",
+		syria: "you're an all star",
+		yemen: "get your game on",
+		sudan: "go play",
+		somalia: "by Smash Mouth"
+	},
+
+	transportation: {
+		libya: "Lorem ipsum",
+		iran: "dolor sit",
+		syria: "amet consectitur",
+		yemen: "Fam quinoa succulents",
+		sudan: "cronut pabst vexillologist butcher",
+		somalia: "keytar lo-fi chillwave tofu synth"
+	},
+
+	information: {
+		libya: "the song goes",
+		iran: "hey now",
+		syria: "you're an all star",
+		yemen: "get your game on",
+		sudan: "go play",
+		somalia: "by Smash Mouth"
+	},
+
+	finance: {
+		libya: "the song goes",
+		iran: "hey now",
+		syria: "you're an all star",
+		yemen: "get your game on",
+		sudan: "go play",
+		somalia: "by Smash Mouth"
+	},
+
+	administrative: {
+		libya: "Lorem ipsum",
+		iran: "dolor sit",
+		syria: "amet consectitur",
+		yemen: "Fam quinoa succulents",
+		sudan: "cronut pabst vexillologist butcher",
+		somalia: "keytar lo-fi chillwave tofu synth"
+	},
+
+	education: {
+		libya: "the song goes",
+		iran: "hey now",
+		syria: "you're an all star",
+		yemen: "get your game on",
+		sudan: "go play",
+		somalia: "by Smash Mouth"
+	},
+
+	arts: {
+		libya: "Lorem ipsum",
+		iran: "dolor sit",
+		syria: "amet consectitur",
+		yemen: "Fam quinoa succulents",
+		sudan: "cronut pabst vexillologist butcher",
+		somalia: "keytar lo-fi chillwave tofu synth"
+	},
+
+	services: {
+		libya: "the song goes",
+		iran: "hey now",
+		syria: "you're an all star",
+		yemen: "get your game on",
+		sudan: "go play",
+		somalia: "by Smash Mouth"
+	},
+
+	public: {
+		libya: "the song goes",
+		iran: "hey now",
+		syria: "you're an all star",
+		yemen: "get your game on",
+		sudan: "go play",
+		somalia: "by Smash Mouth"
+	},
+};
+
+var amounts = {
+	agriculture: {
+		libya: "N/A",
+		iran: "1,347",
+		syria: "473",
+		yemen: "68",
+		sudan: "123",
+		somalia: "190"
+	},
+
+	construction: {
+		libya: "N/A",
+		iran: "8,755",
+		syria: "1,616",
+		yemen: "476",
+		sudan: "492",
+		somalia: "474"
+	},
+
+	manufacturing: {
+		libya: "N/A",
+		iran: "19,081",
+		syria: "2,759",
+		yemen: "1,378",
+		sudan: "4,480",
+		somalia: "7,067"
+	},
+
+	wholesaletrade: {
+		libya: "N/A",
+		iran: "8,306",
+		syria: "1,656",
+		yemen: "408",
+		sudan: "443",
+		somalia: "759"
+	},
+
+	retailtrade: {
+		libya: "N/A",
+		iran: "30,754",
+		syria: "10,722",
+		yemen: "9,286",
+		sudan: "3,077",
+		somalia: "3,984"
+	},
+
+	transportation: {
+		libya: "N/A",
+		iran: "8,530",
+		syria: "1,656",
+		yemen: "527",
+		sudan: "3,815",
+		somalia: "9,343"
+	},
+
+	information: {
+		libya: "N/A",
+		iran: "4,490",
+		syria: "828",
+		yemen: "119",
+		sudan: "714",
+		somalia: "427"
+	},
+
+	finance: {
+		libya: "N/A",
+		iran: "15,938",
+		syria: "1,498",
+		yemen: "340",
+		sudan: "640",
+		somalia: "2,371"
+	},
+
+	administrative: {
+		libya: "N/A",
+		iran: "27,836",
+		syria: "2,720",
+		yemen: "1,088",
+		sudan: "1,600",
+		somalia: "3,889"
+	},
+
+	education: {
+		libya: "N/A",
+		iran: "63,304",
+		syria: "10,328",
+		yemen: "1,310",
+		sudan: "5,661",
+		somalia: "13,090"
+	},
+
+	arts: {
+		libya: "N/A",
+		iran: "15,040",
+		syria: "2,444",
+		yemen: "1,446",
+		sudan: "2,092",
+		somalia: "3,178"
+	},
+
+	services: {
+		libya: "N/A",
+		iran: "12,122",
+		syria: "1,971",
+		yemen: "357",
+		sudan: "714",
+		somalia: "1,707"
+	},
+
+	public: {
+		libya: "N/A",
+		iran: "8,979",
+		syria: "788",
+		yemen: "221",
+		sudan: "788",
+		somalia: "949"
+	},
 };
 
 $(document).ready(function () {
@@ -29,12 +247,16 @@ $(document).ready(function () {
 				var country = $(this);
 				var name = country.attr("id");
 				var content = country.find(".content");
+				var amount = country.find(".amount");
 				var text = responses[select.val()][name];
+				var number = amounts[select.val()][name];
 				country.addClass("active");
+				amount.html(number + " people");
 				content.html(text);
 			});
 		}
 	});
+
 
 	$(".country").on("click", function (e) {
 		e.preventDefault();
@@ -50,4 +272,5 @@ $(document).ready(function () {
 		}
 
 	});
+
 });
